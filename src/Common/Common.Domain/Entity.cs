af1e046857dc0a4dee2b98b5158231ae4eb4de0c
Erick <erickjb93@gmail.com>
2026-09-02 23:34:03 -0300

    ♻️ refactor(domain): tornar setter de Id privado na Entity

--- src/Common/Common.Domain/Entity.cs
+++ src/Common/Common.Domain/Entity.cs
@@ -2,7 +2,7 @@ namespace Common.Domain;
 
 public abstract class Entity<TId>(TId id)
 {
-    public TId Id { get; protected set; } = id;
+    public TId Id { get; private set; } = id;
     public DateTime CreatedAt { get; private set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; private set; }
 
